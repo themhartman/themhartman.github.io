708f35ec3ac18a6b261fb912373c9c2f43610936
Matt Hartman <mrhartman9@gmail.com>
2018-03-18 13:09:15 -0400

    Menu fix, archive link change

--- assets/isotope.settings.js
+++ assets/isotope.settings.js
@@ -1,17 +1,5 @@
-// Archive filtering
-
-// init Isotope
-var $grid = $('.archive').isotope({
+$('.archive').isotope({
   // options
   itemSelector: '.preview',
   layoutMode: 'fitRows'
-});
-// filter items on button click
-$('.filter-button-group').on( 'click', 'a', function() {
-  var filterValue = $(this).attr('data-filter');
-  $grid.isotope({ filter: filterValue });
-});
-$('.button-group a.button').on('click', function(){
-	$('.button-group a.button').removeClass('active');
-	$(this).addClass('active');
 });
